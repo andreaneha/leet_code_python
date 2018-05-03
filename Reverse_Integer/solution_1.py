c82fa79f552b20d1d58daf5d4e511cfe0fa3104c
Andrea Fernandes <aferna06@uoguelph.ca>
2018-05-02 21:27:42 -0700

    my solution final

--- Reverse_Integer/solution_1.py
+++ Reverse_Integer/solution_1.py
@@ -8,6 +8,9 @@ Output: 321
 
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [-2^31,  2^31 - 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
+My solution converts the integer into a string, and then reverses it by using a pivot point and switching the values before and after it. After values get switched, the pivot expands and the loop repeats
+
+
 """
 
 class Solution(object):
@@ -16,8 +19,16 @@ class Solution(object):
             return False
         else:
             return True 
+    def checkNeg(self, x):
+        if(x<0):
+            return True
+        else:
+            return False
 
     def reverseInt(self, x):
+        neg = self.checkNeg(x)
+        if(neg):
+            x*=-1
         intstr = str(x)
         intlen = len(intstr)
         pivot = intlen/2
@@ -32,13 +43,19 @@ class Solution(object):
         for i in range(1, int(intlen/2) +1):
             newstr = intstr[startIndFl+i] + newstr
             newstr = newstr + intstr[startIndCl - i]
-
-        return int(newstr)
+        if(neg):
+            return int(newstr)*(-1)
+        else:
+            return int(newstr)
 
 
     def reverse(self, x):
         if (self.check32int(x)):
-            return self.reverseInt(x)
+            newint = self.reverseInt(x)
+            if(self.check32int(newint)):
+                return newint
+            else:
+                return 0
         else:
             return 0
 
@@ -51,15 +68,16 @@ if(not check32.check32int(2**64) and check32.check32int(6)):
     print("check function works")
 else:
     print("check function does not work")
-
+#check reverseInt method
 checkRev = Solution()
 newstr = checkRev.reverseInt(3069897)
 print(newstr)
 newstr = checkRev.reverseInt(306897)
 print(newstr)
-
+#check reverse method
 newint = checkRev.reverse(6007)
 print(newint)
 newint = checkRev.reverse(2**40)
 print(newint)
-
+newint = checkRev.reverse(-567)
+print(newint)
